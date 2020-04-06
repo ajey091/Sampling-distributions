e511e94f393005facee7dc9386ecd5e62ac626f9
unknown <ajey.091@gmail.com>
2020-04-06 14:11:48 -0400

    Cleaned up

--- Sampling_4.py
+++ Sampling_4.py
@@ -1,12 +1,27 @@
 import numpy as np 
 import scipy.stats
+import matplotlib.pyplot as plt
 
-for n in np.linspace(1,100,5):
-    dist1 = scipy.stats.norm(0.0,1.0)
-    dist2 = scipy.stats.norm(0.0,1.0)
+dist1 = scipy.stats.norm(0.0,1.0)
+dist2 = scipy.stats.norm(0.0,1.0)
+
+def checknorm(n):
     samp1 = dist1.rvs(n)
-    samp2 = dist2.rvs(n)
-    # res1 = scipy.stats.kstest(samp1,'norm')
-    # res2 = scipy.stats.kstest(samp2,'norm')
-    # print (f'pvalue for 1:{res1[1]}')
-    # print (f'pvalue for 2:{res2[1]}')
+    # samp2 = dist2.rvs(n)
+    res1 = scipy.stats.kstest(samp1,'norm')
+    return res1[1]
+
+
+n = np.linspace(2,1000,20)
+stddist = []
+for i in n:
+    buff = []
+    for _ in range(500):
+        buff.append(checknorm(int(i)))
+    stddist.append(np.std(buff))
+
+plt.plot(n,stddist)
+plt.xlabel('Number of samples drawn')
+plt.ylabel('Standard deviation')
+plt.show()
+
